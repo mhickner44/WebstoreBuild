bf8c926348fdd8844cc736c1e22a733171431094
mhickner44 <62121725+mhickner44@users.noreply.github.com>
2020-05-14 02:23:56 -0400

    attempt
    displaying the blue jeans item page on a different link

--- finalWebpage/displayItem.js
+++ finalWebpage/displayItem.js
@@ -1,11 +1,11 @@
 //were going to switch it and display the item display page when 
 //we click on the item on the inventory page
 
-//blue jeans link
+//items link
 blueJeans = document.getElementById("blueJeans");
 tanShorts=document.getElementById("tanShorts");
 //inventory div and itemdisplay div
-bottomItems = document.getElementById("bottomItems");
+bottomItems = document.getElementById("inventory");
 displayItem = document.getElementById("displayItem");
 //interchangeable information
 image1= document.getElementById("image1");
@@ -20,14 +20,6 @@ pdescription= document.getElementById("pDescription");
 blueJeans.addEventListener("click", blueJeansDisplay);
 tanShorts.addEventListener("click",tanShortsDisplay);
 
-//JSON stuff
-/*examplejson='{"image1":"../threadsImages/mensBottoms/frontBlueJeans.jpg","image2":"../threadsImages/mensBottoms/backBlueJeans.jpg",'+
-       ' "itemTitle":"Blue Jeans","Price":"$15.00","pdescription":"Some nice blue jeans. Made of denim in the USA."}';
-
-exampleInfo=JSON.parse(examplejson);
-*/
-
-//correct JSON stuff
 // store the URL for the file we want to get info from
 requestURL  = 'https://mhickner44.github.io/WebstoreBuild/finalWebpage/items.json';
 request = new XMLHttpRequest();  // build something that can go out and request files from the web
@@ -36,16 +28,7 @@ request.open('GET', requestURL); // build the GET request
 request.responseType ='json';
 request.send();  //send the GET request, but it takes some time to get back
 
-// wait for the request to complete
-/*
-request.onload = function() { // stop until the request is done
-    items = request.response; //this is the data, so let's store it in a variable
-    console.log(items);
-    blueJeansDisplay(items);//this line sends the information to the function
-}
-*/
-//i believe each image will have to have a different hide display because 
-//the code for the specific item will go in there
+
 
 
 
